b8fc0b2010afaf890098a1ac20cfeb0bf1195225
knarr <wayfaringknarr@gmail.com>
2014-01-25 05:27:03 -0500

    Added flickr

--- heatmap/js/main.js
+++ heatmap/js/main.js
@@ -5,19 +5,25 @@ $(function() {
     var container = document.getElementById("map");
 
     var map = new nokia.maps.map.Display(container, {
-	compontents: [],
+	components: [],
 	zoomLevel: 14,
 	center: [lat, lng]
     });
 
     map.overlays.clear(); // Clear any existing overlays on the map
     getInsta(lat, lng, function (data) {
-	console.log(data);
+	heatMap(map,data);
+    });
+    /* Works but has near identical data to instagram
+    getFlickr(lat, lng, function (data) {
 	heatMap(map, data);
     });
+    */ 
+    /* Doesn't work
+    getTwitter(lat, lng, function (data) {
 
-    
-    
+    });
+    */
 });
 
 // Draw a heatmap ontop of the given map, using the data as src
@@ -33,6 +39,56 @@ function heatMap(map, data) {
 }
 
 
+function getTwitter(lat, lng, callback) {
+    var app_key = 'UQVGvAiVnMSJpqdefj44Jw';
+    var access_token = 'AAAAAAAAAAAAAAAAAAAAABRMVwAAAAAAnE6%2F41mZ20HV2LVadOufnqih%2Fc8%3DAP8Dhd8DkaKCFhJ1Nn7ZedG5TmU54dRJ865U26ByLgU0oIlkuo';
+    
+    $.get('https://api.twitter.com/1.1/geo/search.json' +
+	  '?lat=' + lat + '&long=' + lng +
+	  '&accuracy=1000' + //1km radius,
+	  function () {}, 'jsonp').done(
+	      function (data) {
+		  console.log(data);
+	      });
+    
+}
+
+function getFlickr(lat, lng, callback) {
+
+    var api_key = '58a5bac6c4ba89f8bf7fd8485ea77f30';
+    var api_secret = 'f79dc83eed4a187b';
+
+    photo_data = [];
+
+    $.getJSON('http://api.flickr.com/services/rest/' +
+	      '?method=flickr.photos.search' +
+	      '&format=json' +
+	      '&api_key=' + api_key +
+	      '&lat=' + lat + '&lon=' + lng +
+	      '&radius=1&has_geo=1&jsoncallback=?' +
+	      '&per_page=100', function (data) {
+		  data = data.photos.photo;
+		  var len = data.length;
+		  for (var i = 0; i < len; i += 1) {
+		      $.getJSON('http://api.flickr.com/services/rest/' +
+			'?method=flickr.photos.geo.getLocation&format=json' +
+			'&api_key=' + api_key + '&jsoncallback=?' +
+			'&photo_id=' + data[i].id, function (pdata) {
+			    photo_data.push({
+				'latitude': pdata.photo.location.latitude,
+				'longitude' : pdata.photo.location.longitude
+			    });
+			    if (photo_data.length == len) {
+				callback(photo_data);
+			    }
+			});
+		  }
+	      });
+	  
+        
+}
+
+
 // Gets the location data for photos around lat,lng
 // apparently gets the 500 most recent photos taken near the center of the map
 // callback is called when the data has been loaded
@@ -43,7 +99,8 @@ function getInsta(lat, lng, callback) {
 
     $.get('https://api.instagram.com/v1/media/search?' +
 	  'lat=' + lat + '&lng=' + lng +
-	  '&count=100' + 
+	  '&count=100' +
+	  '&distance=7000' +
 	  '&access_token=' + insta_access_token,
 	  function () {}, 'jsonp').done(function (data) {
 	      photo_data = []; // empty photo data array
